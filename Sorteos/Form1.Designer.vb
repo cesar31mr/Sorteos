b80485b7b61205b18c70cdaf937b3bc5c7d3c9d4
Cesar Martinez <cesar31mr@google.com>
2019-08-28 19:59:49 -0500

    Agregar archivos de proyecto.

---
+++ Sorteos/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtInferior = New System.Windows.Forms.TextBox()
+        Me.txtSuperior = New System.Windows.Forms.TextBox()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.txtHistorial = New System.Windows.Forms.TextBox()
+        Me.lblResultados = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Límite Inferior"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(223, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Límite Superior"
+        '
+        'txtInferior
+        '
+        Me.txtInferior.Location = New System.Drawing.Point(37, 42)
+        Me.txtInferior.Name = "txtInferior"
+        Me.txtInferior.Size = New System.Drawing.Size(130, 20)
+        Me.txtInferior.TabIndex = 2
+        Me.txtInferior.Text = "0"
+        '
+        'txtSuperior
+        '
+        Me.txtSuperior.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSuperior.Location = New System.Drawing.Point(226, 42)
+        Me.txtSuperior.Name = "txtSuperior"
+        Me.txtSuperior.Size = New System.Drawing.Size(130, 20)
+        Me.txtSuperior.TabIndex = 3
+        Me.txtSuperior.Text = "100"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtResultado.BackColor = System.Drawing.Color.Coral
+        Me.txtResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResultado.ForeColor = System.Drawing.Color.DarkGreen
+        Me.txtResultado.Location = New System.Drawing.Point(111, 120)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.ReadOnly = True
+        Me.txtResultado.Size = New System.Drawing.Size(169, 41)
+        Me.txtResultado.TabIndex = 4
+        Me.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(168, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Resultado"
+        '
+        'btnIniciar
+        '
+        Me.btnIniciar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnIniciar.Location = New System.Drawing.Point(158, 78)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(75, 23)
+        Me.btnIniciar.TabIndex = 6
+        Me.btnIniciar.Text = "Iniciar"
+        Me.btnIniciar.UseVisualStyleBackColor = True
+        '
+        'txtHistorial
+        '
+        Me.txtHistorial.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtHistorial.Location = New System.Drawing.Point(12, 209)
+        Me.txtHistorial.Multiline = True
+        Me.txtHistorial.Name = "txtHistorial"
+        Me.txtHistorial.ReadOnly = True
+        Me.txtHistorial.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtHistorial.Size = New System.Drawing.Size(367, 141)
+        Me.txtHistorial.TabIndex = 7
+        '
+        'lblResultados
+        '
+        Me.lblResultados.AutoSize = True
+        Me.lblResultados.Location = New System.Drawing.Point(12, 180)
+        Me.lblResultados.Name = "lblResultados"
+        Me.lblResultados.Size = New System.Drawing.Size(72, 13)
+        Me.lblResultados.TabIndex = 8
+        Me.lblResultados.Text = "Resultados: 0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(391, 362)
+        Me.Controls.Add(Me.lblResultados)
+        Me.Controls.Add(Me.txtHistorial)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.txtSuperior)
+        Me.Controls.Add(Me.txtInferior)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Sorteo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtInferior As TextBox
+    Friend WithEvents txtSuperior As TextBox
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents txtHistorial As TextBox
+    Friend WithEvents lblResultados As Label
+End Class
---
+++ Sorteos/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub BtnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        Try
+            If IsNumeric(txtInferior.Text) And IsNumeric(txtSuperior.Text) Then
+                Dim nmbAleatorio As New Random()
+                Dim inferior As Integer = CInt(txtInferior.Text)
+                Dim Superior As Integer = CInt(txtSuperior.Text)
+                Dim historico As String = String.Empty
+
+                historico = txtHistorial.Text
+                txtResultado.Text = Convert.ToString(nmbAleatorio.Next(inferior, Superior + 1))
+
+                If txtHistorial.MaxLength < (txtResultado.Text.Length + historico.Length) Then
+                    txtHistorial.Text = txtResultado.Text
+                Else
+                    txtHistorial.Text = String.Format("{0}{1}{2}", txtResultado.Text, vbCrLf, txtHistorial.Text)
+                End If
+
+                lblResultados.Text = String.Format("Resultados: {0}", txtHistorial.Text.Split(vbCrLf).Length - 1)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+End Class
